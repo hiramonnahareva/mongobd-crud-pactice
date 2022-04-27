983de35113e7cfdb06231b334600b0e5ccab07a9
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-04-27 14:14:57 +0600

    create, read, update, delete

---
+++ __staps.js
@@ -0,0 +1,38 @@
+/**
+ * 1. create a node server with 5 steps
+ * 1.1. create folder
+ * 1.2. npm init 
+ * 1.3. npm i express cors index.js
+ * 1.5. create index.js
+ * 1.6. use 5 stps to create a node server
+ * ----------------------
+ * create Atlas Account
+ * ----------------------
+ * 1. sign up. google access
+ * 2. create cluster
+ * 3. create user dbUser and password
+ * 4. Network Access --> ip address: allow all
+ * 5. database > connect > code copy paste in index.js
+ * ---------------
+ * CRAD Operation
+ * ---------------
+ * 1. node mongodb CRAD > Fundamentals 
+ * 2. create async run function
+ * ----------------------------------------------
+ * Integrate Sanding data from client to server
+ * ----------------------------------------------
+ * 1. Client side: create form
+ * 2. on submit get form data and create user object
+ * 3. on sever: create user POST method to receive data on the backend
+ * 4. on client side: set fetch with POST, header, body.
+ * 5. Make sure you return a json from POST API
+ * ----------------------------------------
+ * LOAD Data to the client side
+ * ----------------------------------------
+ * 1. create get API on the server
+ * 2. create a query objects
+ * 3. collection.find (query)
+ * 4. cursor.toArray()
+ * 5. return the result
+ */
+
---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const cors = require ('cors');
+const Object = require('mongodb').ObjectId;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+// use middleware
+
+app.use (cors());
+app.use (express.json ());
+// user: dbuse1 
+//password: g31HEqwvcoO8UUL8
+
+
+const uri = "mongodb+srv://dbuse1:g31HEqwvcoO8UUL8@cluster0.u3hia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try {
+        await client.connect();
+        const userCollection = client.db('foodExperss').collection('user');
+
+        app.get ('/user', async(req, res)=> {
+            const query = {} ;
+            const cursor = userCollection.find (query) ;
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+        // update user 
+        app.put('/user/:id', async(req, res)=> {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = {_id : Object(id)};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                }
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+
+        } )
+        // post user : add a new user
+        app.post('/user', async(req, res) =>{
+            const newUser = req.body;
+            console.log ('adding new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send({result})
+        });
+        app.get ('/user/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.findOne(query);
+            res.send (result);
+        })
+
+        // delete a user
+        app.delete ('/user/:id' , async (req, res)=> {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send (result);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+    
+
+
+app.get ('/', (req, res)=> {
+    res.send('Running My Node CURD Server');
+});
+app.listen (port , ()=>{
+    console.log ('CRUD server is running');
+})
